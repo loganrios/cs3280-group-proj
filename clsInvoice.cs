dbfc89a8ae153a32cc55cb6e6600b5d057d93921
athurdek <54721871+athurdek@users.noreply.github.com>
2020-11-14 19:50:46 -0700

    Add files via upload

---
+++ clsInvoice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace group_proj
+{
+    public class clsInvoice
+    {
+        public int iInvoiceNum { get; set; }
+        public DateTime dtInvoiceDate { get; set; }
+        public int dTotalCost { get; set; }
+
+
+        public clsInvoice (int iInvoiceNum, DateTime dtInvoiceDate, int dTotalCost)
+        {
+            this.iInvoiceNum = iInvoiceNum;
+            this.dtInvoiceDate = dtInvoiceDate;
+            this.dTotalCost = dTotalCost;
+        }
+
+
+    }
+}
