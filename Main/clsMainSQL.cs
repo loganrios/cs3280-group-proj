22b893292f0921f397de7f4db812e760ec65dfe5
loganrios <loganrios1@gmail.com>
2020-11-14 20:14:03 -0700

    Add clsItem and some Main SQL

--- Main/clsMainSQL.cs
+++ Main/clsMainSQL.cs
@@ -8,5 +8,42 @@ namespace group_proj.Main
 {
     class clsMainSQL
     {
+        public string DeleteLineItemsFromInvoiceID(int InvoiceID)
+        {
+            return
+                "DELETE FROM LineItems" +
+                "WHERE InvoiceNum = " + InvoiceID.ToString() + "; ";
+        }
+
+        public string DeleteInvoice(int InvoiceID)
+        {
+            return
+                "DELETE FROM Invoices" +
+                "WHERE InvoiceNum = " + InvoiceID.ToString() + "; ";
+        }
+
+        public string InsertLineItemForInvoice(int InvoiceID, int Quantity, string ItemCode)
+        {
+            return
+                "INSERT INTO LineItems (InvoiceNum, LineItemNum, ItemCode) " +
+                "Values(" + InvoiceID.ToString() + ", " +
+                Quantity.ToString() + ", " +
+                "'" + ItemCode + "');";
+        }
+
+        public string InsertNewInvoice(DateTime InvoiceDate, int TotalCost)
+        {
+            return
+                "INSERT INTO Invoices(InvoiceDate, TotalCost) " +
+                "Values('#4/13/2018#', 100)";
+        }
+
+        public string SelectInvoiceData(int InvoiceID)
+        {
+            return
+                "SELECT InvoiceNum, InvoiceDate, TotalCost" +
+                "FROM Invoices" +
+                "WHERE InvoiceNum = " + InvoiceID.ToString() + "; ";
+        }
     }
 }
